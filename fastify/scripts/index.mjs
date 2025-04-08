1b620ad3b8c4d21d25e97b3d5a87b961eda668a3
u2re-dev <c24b@u2re.ru>
2025-04-08 21:35:10 +0700

    forcely redirect into https

--- fastify/scripts/index.mjs
+++ fastify/scripts/index.mjs
@@ -1,5 +1,19 @@
 import { options } from "./router.mjs";
 import server from "./server.mjs";
+import { Worker } from 'worker_threads';
+import path from 'path';
+
+//
+const runRedirectServer = async () => {
+    const redirectWorker = new Worker('./scripts/redirect.mjs', { execArgv: ['--experimental-modules'] });
+    redirectWorker.on('online', () => console.log('Redirect server is running in a separate thread.'));
+    redirectWorker.on('error', (err) => console.error('Error in redirect server:', err));
+    redirectWorker.on('exit', (code) => console.log(`Redirect server exited with code ${code}`));
+    return redirectWorker;
+};
+
+//
+runRedirectServer()?.catch?.(console.warn.bind(console));
 
 //
 const app = await server();
