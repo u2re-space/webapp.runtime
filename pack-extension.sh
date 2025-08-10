f6e16d90ab85f395b527143f2cf1a99cd11973a0
u2re-dev <c24b@u2re.ru>
2025-08-10 16:09:35 +0700

    Docs

--- pack-extension.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir ./webext/
-openssl genrsa -out ./webext/webapp.pem 2048
-web-ext build --source-dir=./frontend --artifacts-dir=./webext/ --overwrite-dest=true -n webapp.xpi
-cd ./frontend && zip -r -9 - . | crx3 -p ../webext/webapp.pem -o ../webext/webapp.crx && cd ../
