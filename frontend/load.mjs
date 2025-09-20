4d8bbd8160df351c70a898f98889b3146adb555e
u2re-dev <c24b@u2re.ru>
2025-09-20 08:15:06 +0700

    Hard Mode

---
+++ frontend/load.mjs
@@ -0,0 +1,14 @@
+import { placeCSSCompatWarning } from "./vital.mjs";
+const _SUB   = location?.hostname?.split?.(".")?.[0]?.trim?.();
+const APP_JS = "./apps/OS/app.js";//_SUB == "print" ? "./print/app.js" : "./app/app.js";
+const SW_JS  = "./pwa/service.mjs";
+
+//
+if (placeCSSCompatWarning()) {
+    (async()=>{
+        if (typeof navigator != "undefined") {
+            await navigator?.serviceWorker?.register?.(new URL(SW_JS, import.meta.url).href, {scope: "/"})?.catch?.(console.warn.bind(console));
+        }
+        import(`${APP_JS}`)?.then?.((m)=>m?.default?.(document?.querySelector?.("#viewport")))?.catch?.(console.error.bind(console));
+    })();
+}
