8301ec8019126eed31a2c90200413d7d7269eafa
L2NE-dev <conway24b@gmail.com>
2026-04-09 09:05:47 +0700

    Transition to Vite 8...

--- cwsp/android/app/src/main/java/space/u2re/cwsp/MainActivity.java
+++ cwsp/android/app/src/main/java/space/u2re/cwsp/MainActivity.java
@@ -1,5 +1,21 @@
 package space.u2re.cwsp;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+/**
+ * Debug builds: allow Chrome/VS Code attach to the Capacitor WebView over ADB
+ * ({@code adb forward tcp:9222 localabstract:webview_devtools_remote_<pid>}), see
+ * {@code runtime/cwsp/scripts/adb-forward-webview-debug.sh}.
+ */
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (space.u2re.cwsp.BuildConfig.DEBUG) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+    }
+}
