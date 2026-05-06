655fa3591634a2eef20dc1f37546de00e9eed8a2
L2NE-dev <conway24b@gmail.com>
2026-05-06 17:56:29 +0700

    Markdown viewer not able to fix...

--- cwsp/endpoint/frontend/index.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyInstance } from "fastify";
-import registerFastifyRoutes from "../server/fastify/router.ts";
-
-export const registerWebPlugin = async (publicApp: FastifyInstance, context: any) => {
-    // Register the unified fastify frontend routes
-    await registerFastifyRoutes(publicApp, {});
-};
